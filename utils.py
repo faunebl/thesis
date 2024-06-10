6bbfc8acfcee0720a0fa9eb2956d51135f48b849
faunebl <faunebl@protonmail.com>
2024-06-11 00:04:49 +0200

    adding cosine similarity & dot product

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import polars as pl 
 
 def display_polars(frame: pl.DataFrame) -> None:
-    with pl.Config(fmt_str_lengths= 1000, set_tbl_cols= -1, set_tbl_rows= -1):
+    with pl.Config(fmt_str_lengths= 100000, set_tbl_cols= -1, set_tbl_rows= -1):
         print(frame)
     return None
