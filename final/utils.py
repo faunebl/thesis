d6a2a406af96144a0af78a1fa3dec0100ccf9809
faunebl <faunebl@protonmail.com>
2024-06-11 23:39:34 +0200

    test

--- final/utils.py
+++
@@ -1,6 +0,0 @@
-import polars as pl 
-
-def display_polars(frame: pl.DataFrame) -> None:
-    with pl.Config(fmt_str_lengths= 1000, set_tbl_cols= -1, set_tbl_rows= -1):
-        print(frame)
-    return None
